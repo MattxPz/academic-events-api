e1b4d80ee4340c8cb7c01545b26474fba6b40208
Tigre <cuji309@gmail.com>
2026-07-26 17:58:18 -0500

    feat(repositories): agrega repositorios de categorias, eventos y sesiones

---
+++ src/main/java/ec/edu/ups/academicevents/categories/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package ec.edu.ups.academicevents.categories.repository;
+
+import ec.edu.ups.academicevents.categories.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>, JpaSpecificationExecutor<Category> {
+
+    boolean existsByNameIgnoreCase(String name);
+
+    boolean existsByNameIgnoreCaseAndIdNot(String name, Long id);
+}
